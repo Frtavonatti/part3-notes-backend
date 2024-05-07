45f60c7982adada73eb6723c061f7426d22f89c1
frtavonatti <frtavonatti@gmail.com>
2024-05-07 18:27:04 +0100

    Implementación de errorHandler

--- index.js
+++ index.js
@@ -1,30 +1,74 @@
-require('dotenv').config()
+// require('dotenv').config()
+// const express = require('express')
+// const cors = require('cors')
+// const app = express()
+// const Note = require('./models/note')
+
+// app.use(express.json())
+// app.use(express.static('dist'))
+// app.use(cors())
+
 const express = require('express')
-const cors = require('cors')
 const app = express()
+require('dotenv').config()
+
 const Note = require('./models/note')
 
-app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 //No funcional
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
-})
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello World</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
+  .catch(error => next(error))
 })
 
-//No funcional
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    Note.findById(id).then(note => {
-      response.json(note)
+  const id = request.params.id
+  Note.findById(id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).send({error: 'malformatted id'})
     })
 })
 
@@ -45,15 +89,20 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
-//No funcional
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  const id = request.params.id
+  Note.findByIdAndDelete(id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
