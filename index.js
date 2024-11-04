2312800997e95e759a19eb643451e298fdfbcdb2
Frtavonatti <frtavonatti@gmail.com>
2024-11-04 17:16:06 +0000

    Part3a en proceso: Ejercicios 3.1-3.6 terminados

---
+++ index.js
@@ -0,0 +1,101 @@
+const express  = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>')
+})
+
+app.get('/info', (req, res) => {    
+    res.send(`
+        <h3>Phonebook has info for ${persons.length} people</h3>
+        <p>Time: ${new Date().toString()} </p>
+        `)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === Number(id))
+
+  person 
+  ? res.json(person)
+  : res.status(404).json({ error: 'The ID that you requested does not exist' })
+})
+
+// Handling POST Requests
+const createNewID = () => {
+  const maxID = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0
+  const newID = maxID + 1
+  return newID
+}
+
+app.post('/api/persons', (req, res) => {
+  const newName = req.body.name
+  const newNumber = req.body.number
+
+  if (!newName || !newNumber) {
+    res.status(400).json({ error: 'Name and Number are required params' })
+    console.error('Name and Number are required params')
+    return
+  } else if (persons.some(person => person.name === newName)) {
+    res.status(400).json({ error: 'Name must be unique' })
+    console.error('Name must be unique')
+    return
+  }
+
+  const newPerson = {
+    id: createNewID(),
+    name: newName, 
+    number: newNumber
+  }
+
+  persons = persons.concat(newPerson)
+  res.status(201).json(persons)
+})
+
+// Handling DELETE Requests
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const personToDelete = persons.find(person => person.id === id)
+  
+  if (personToDelete) {
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).json({ message: `${personToDelete.name} deleted` })
+    console.log(`${personToDelete.name} deleted`)
+  } else {
+    res.status(404).json({ error: 'The ID that you requested does not exist' })
+    console.error('The ID that you requested does not exist')
+  }
+})
+
+const PORT = 3000 
+app.listen(PORT,  () => {
+    console.log(`Server running on port ${PORT}`);
+})
