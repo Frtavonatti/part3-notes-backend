b87ccc279d706bfa6d3d089ca8a4efd88ba16b65
Frtavonatti <frtavonatti@gmail.com>
2024-11-08 17:13:44 +0000

    Part3d finalizada: Ejercicio 3.22 terminado

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-morgan.token('content', (req, res) => JSON.stringify(req.body))
+morgan.token('content', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content '))
 
 // MODELS
@@ -19,103 +19,103 @@ const Person = require('./models/person')
 // GET all
 app.get('/api/persons', (req, res) => {
     Person.find({})
-      .then(persons => {
-        res.json(persons)
-      })
+        .then(persons => {
+            res.json(persons)
+        })
 })
 
 // GET person by ID
 app.get('/api/persons/:id', (req, res, next) => {
-  const id = req.params.id
-  Person.findById(id)
-    .then(person => {
-      if (person) {
-        res.json(person);
-      } else {
-        res.status(404).json({ error: 'The ID that you requested does not exist' });
-      }
-    })
-    .catch(error => next(error)); 
+    const id = req.params.id
+    Person.findById(id)
+        .then(person => {
+            if (person) {
+                res.json(person)
+            } else {
+                res.status(404).json({ error: 'The ID that you requested does not exist' })
+            }
+        })
+        .catch(error => next(error))
 })
 
 
 // POST
 app.post('/api/persons', (req, res, next) => {
-  const newName = req.body.name
-  const newNumber = req.body.number
+    const newName = req.body.name
+    const newNumber = req.body.number
 
-  // Reemplazado por validación en Schema
-  // if (!newName || !newNumber) {
-  //   res.status(400).json({ error: 'Name and Number are required params' })
-  //   console.error('Name and Number are required params')
-  //   return
-  // } 
-  
-  const newPerson = new Person({
-    name: newName, 
-    number: newNumber
-  })
+    // Reemplazado por validación en Schema
+    // if (!newName || !newNumber) {
+    //   res.status(400).json({ error: 'Name and Number are required params' })
+    //   console.error('Name and Number are required params')
+    //   return
+    // }
 
-  newPerson.save()
-    .then(savedPerson => {
-      console.log(`${savedPerson} successfully added`)
-      res.status(201).json(savedPerson)
+    const newPerson = new Person({
+        name: newName,
+        number: newNumber
     })
-    .catch(error => next(error))
+
+    newPerson.save()
+        .then(savedPerson => {
+            console.log(`${savedPerson} successfully added`)
+            res.status(201).json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 
 //PUT
 app.put('/api/persons/:id', (req, res, next) => {
-  const id = req.params.id
-  const {name, number} = req.body
+    const id = req.params.id
+    const { name, number } = req.body
 
-  Person.findByIdAndUpdate(
-    id, 
-    { name, number }, 
-    { new: true, runValidators: true, context: 'query' }
-  )
-    .then(updatedPerson => {
-      console.log(`${updatedPerson} information changed successfully`)
-      res.status(200).json(updatedPerson)
-    })
-    .catch(error => next(error))
+    Person.findByIdAndUpdate(
+        id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedPerson => {
+            console.log(`${updatedPerson} information changed successfully`)
+            res.status(200).json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 
 // DELETE
 app.delete('/api/persons/:id', (req, res, next) => {
-  const id = req.params.id
+    const id = req.params.id
 
-  Person.findByIdAndDelete(id)
-    .then(personToDelete => {
-      if (personToDelete) {
-        res.status(204).json(personToDelete);
-        console.log(`${personToDelete.name} deleted`);
-      } else {
-        res.status(404).json({ error: 'The ID that you requested does not exist' });
-      }
-    })
-    .catch(error => next(error)); 
+    Person.findByIdAndDelete(id)
+        .then(personToDelete => {
+            if (personToDelete) {
+                res.status(204).json(personToDelete)
+                console.log(`${personToDelete.name} deleted`)
+            } else {
+                res.status(404).json({ error: 'The ID that you requested does not exist' })
+            }
+        })
+        .catch(error => next(error))
 })
 
 
 // MIDLEWARE
 //Error Handler
 app.use((error, req, res, next) => {
-  console.error(error.message)
-  
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {    
-    return res.status(400).json({ error: error.message })  
-  }
+    console.error(error.message)
 
-  next(error)
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
+    }
+
+    next(error)
 })
 
 // PORT CONECTION
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 app.listen(PORT,  () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -6,56 +6,56 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => { 
+    .then(() => {
         console.log('connected to mongoDB')
-})  .catch(error => {    
-    console.log('error connecting to MongoDB:', error.message)  
-})
+    })  .catch(error => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true
-  },
-  number: {
-    type: String,
-    minLength: 8,
-    // Validador personalizado
-    validate: {
-      validator: function(value) {
-        // Dividir el número en partes usando el guion como separador
-        const parts = value.split('-');
-        // Verificar que haya exactamente dos partes
-        if (parts.length !== 2) {
-          return false;
-        }
-        // Verificar que la primera parte tenga 2 o 3 dígitos
-        const part1 = parts[0];
-        if (part1.length < 2 || part1.length > 3 || isNaN(part1)) {
-          return false;
-        }
-        // Verificar que la segunda parte tenga solo dígitos
-        const part2 = parts[1];
-        if (isNaN(part2)) {
-          return false;
-        }
-        return true;
-      },
-      message: props => `${props.value} is not a valid phone number!`
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
     },
-    required: true
-  }
+    number: {
+        type: String,
+        minLength: 8,
+        // Validador personalizado
+        validate: {
+            validator: function(value) {
+                // Dividir el número en partes usando el guion como separador
+                const parts = value.split('-')
+                // Verificar que haya exactamente dos partes
+                if (parts.length !== 2) {
+                    return false
+                }
+                // Verificar que la primera parte tenga 2 o 3 dígitos
+                const part1 = parts[0]
+                if (part1.length < 2 || part1.length > 3 || isNaN(part1)) {
+                    return false
+                }
+                // Verificar que la segunda parte tenga solo dígitos
+                const part2 = parts[1]
+                if (isNaN(part2)) {
+                    return false
+                }
+                return true
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
+})
 
 const Person = mongoose.model('Person', personSchema)
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = Person
